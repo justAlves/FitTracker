49c691da1d1e3870c210cf2b38eaa2e716615eff
justAlves <iamalves.jpg@gmail.com>
2023-08-01 13:36:36 -0300

    feat: first commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Router, { push } from "svelte-spa-router";
+  import { conditionsFailHandler, routeLoadingHandler, routes } from "./routes";
+  import LoggedInUser from "$stores/LoggedInUser";
+  import lunaImg from "$assets/Luna.jpg";
+  import Header from "$components/Header/Header.svelte";
+  import { tick } from "svelte";
+
+  const userMock = {
+    name: "João Silva",
+    image: lunaImg,
+  };
+</script>
+
+  <Router
+    {routes}
+    on:routeLoading={routeLoadingHandler}
+    on:conditionsFailed={conditionsFailHandler}
+  />
+
+  
---
+++ src/routes.ts
@@ -0,0 +1,63 @@
+
+import NotAuthorized from "$pages/NotAuthorized/NotAuthorized.svelte";
+import NotFound from "$pages/NotFound/NotFound.svelte";
+import { isUserLoggedIn } from "$policies/auth";
+import { type ConditionsFailedEvent, type RouteLoadingEvent, replace } from "svelte-spa-router";
+import { wrap } from "svelte-spa-router/wrap";
+
+export const routes = new Map();
+
+routes.set(
+  "/",
+  wrap({
+    component: Home,
+  })
+);
+routes.set(
+  "/exercise",
+  wrap({
+    component: Profile,
+  })
+);
+routes.set(
+  "/settings",
+  wrap({
+    component: Settings,
+  })
+);
+routes.set(
+  "/not-authorized",
+  wrap({
+    component: NotAuthorized,
+  })
+);
+routes.set(
+  "*",
+  wrap({
+    component: NotFound,
+  })
+);
+
+export function routeLoadingHandler({ detail }: RouteLoadingEvent): void {
+  // If not hash based, redirect to hash based path
+  if (!window.location.hash.startsWith("#/")) {
+    // Save query string
+    const { search } = window.location;
+
+    // Remove any paths and querystrings
+    window.history.replaceState(null, "", `${window.location.origin}${window.location.pathname}`);
+
+    // Go to location with querystring
+    replace(detail.location + search).catch((e) => {
+      throw e;
+    });
+  }
+}
+
+/**
+ * On some condition fail this function will be triggered, performing routes change
+ */
+export async function conditionsFailHandler(event: ConditionsFailedEvent): Promise<void> {
+  const { route } = event.detail;
+  await replace(`/not-authorized?route=${route as string}`);
+}
