45e3c731c1fd7490782630a08461d76c072c1cfd
justAlves <iamalves.jpg@gmail.com>
2023-08-05 17:35:44 -0300

    feat: :sparkles: sending custom notifications

--- src/policies/notificationController.ts
+++ src/policies/notificationController.ts
@@ -15,30 +15,77 @@ interface Notification {
   waterGoal?: number;
 }
 
-const { initialHour, finalHour, waterPerNotification, exercised, exerciseGoal, drinkedWater, waterGoal } = store.get();
-
 export async function callWaterNotification({
   initialHour,
   finalHour,
   waterPerNotification,
   drinkedWater,
-  waterGoal }: Notification) {
+  waterGoal
+}: Notification) {
+
+  const hour = new Date().getHours();
+
+  if (drinkedWater >= waterGoal || initialHour > hour || finalHour < hour) return;
+
   try {
     await LocalNotifications.schedule({
       notifications: [
         {
-          title: "Hora de beber água!",
-          body: `Você bebeu ${drinkedWater}ml de ${waterGoal}ml hoje. Beba mais ${waterPerNotification}ml!`,
+          title: "Hidrate-se: É hora de beber água!",
+          body: `A hidratação é essencial para o seu bem-estar. Que tal beber ${waterPerNotification}ml de água agora e ficar mais próximo da sua meta diária? Seu corpo agradecerá!`,
           id: 1,
           schedule: {
-            at: new Date(Date.now() + 1000 * 5),
+            at: new Date(Date.now() + 1000 * 60 * 15),
+            allowWhileIdle: true,
+            repeats: true,
+          },
+          sound: null,
+          attachments: null,
+          actionTypeId: "",
+          extra: {
+            route: "/"
+          },
+        },
+      ],
+    }).then(() => {
+      console.log('Notification set');
+    });
+
+    await LocalNotifications.addListener('localNotificationActionPerformed', async (notification) => {
+      const route = notification.notification.extra.route;
+      await push(route);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function callExerciseNotification({
+  initialHour,
+  finalHour,
+  exercised,
+  exerciseGoal }: Notification) {
+
+  const hour = new Date().getHours();
+
+  if (exercised >= exerciseGoal || initialHour > hour || finalHour < hour) return; // Não notificar se já atingiu a meta ou se não estiver no horário
+
+  try {
+    await LocalNotifications.schedule({
+      notifications: [
+        {
+          title: "Faça uma pausa para o bem da sua saúde!",
+          body: `Que tal uma pausa para um alongamento ou uma caminhada rápida? Cuide do seu corpo e mente agora mesmo!`,
+          id: 1,
+          schedule: {
+            at: new Date(Date.now() + 1000 * 2),
             allowWhileIdle: true,
           },
           sound: null,
           attachments: null,
           actionTypeId: "clique", // Defina um ID de ação exclusivo para identificar essa ação
           extra: {
-            route: "/"
+            route: "/exercise"
           },
         },
       ],
@@ -52,5 +99,6 @@ export async function callWaterNotification({
     console.log(error);
   }
 }
+
 
 
